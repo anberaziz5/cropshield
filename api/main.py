d822f68e5eab42ce1a4a6a360d14fc6272a8b535
Anber Aziz <anberaziz@users.noreply.huggingface.co>
2026-07-09 11:08:24 +0000

    Update api/main.py

--- api/main.py
+++ api/main.py
@@ -45,7 +45,7 @@ async def predict(file: UploadFile = File(...)):
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     # Run inference with a low confidence threshold
-    results = MODEL.predict(img_rgb, conf=0.05, device='cpu')[0]
+    results = MODEL.predict(img_rgb, conf=0.25, device='cpu')[0]
 
     diseases = []
     confidences = []
